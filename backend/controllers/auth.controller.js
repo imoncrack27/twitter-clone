d09b9dbd0db691ed4456ce5a445c8b61ce47bf1d
imoncrack27 <mingodrexell321@gmail.com>
2025-04-16 21:21:50 +0800

    Login controller is now operational on Postman

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -61,7 +61,34 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.json({ data: "login Page" });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    generateTokenAndSetCookie(user.id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+      followers: user.followers,
+      following: user.following,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const logout = async (req, res) => {

d50f07990e51164286d1f402ba43a2e7f53e7cd5
imoncrack27 <mingodrexell321@gmail.com>
2025-04-16 17:20:36 +0800

    .env wont be seen hehe

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.json({ data: "signup Page" });
+};
+
+export const login = async (req, res) => {
+  res.json({ data: "login Page" });
+};
+
+export const logout = async (req, res) => {
+  res.json({ data: "logout Page" });
+};

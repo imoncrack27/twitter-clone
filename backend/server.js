29deec69e0cf4aa24dc422d187ccea19c0e5fcca
imoncrack27 <mingodrexell321@gmail.com>
2025-04-18 16:17:22 +0800

    follow/Unfollow is working in postman also

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(cookieParser()); // to parse cookies
 
 //Route middleware
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

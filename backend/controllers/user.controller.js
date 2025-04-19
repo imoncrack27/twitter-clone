5fdb9a2fcf50641b847182489ccb145e9ccf20f3
imoncrack27 <mingodrexell321@gmail.com>
2025-04-19 10:11:00 +0800

    notification is up and running (Notifcation model and controller finished)

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
 export const getUserProfile = async (req, res) => {
@@ -36,20 +37,22 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
+      //TODO: return the id of the user as a response
       res.status(200).json({ message: "User unfollowed successfully" });
     } else {
       // Follow the user
       await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
       // Send notification to the user
-      /*const newNotification = new Notification({
+      const newNotification = new Notification({
         type: "follow",
         from: req.user._id,
         to: userToModify._id,
       });
 
-      await newNotification.save();*/
+      await newNotification.save();
 
+      //TODO: return the id of the user as a response
       res.status(200).json({ message: "User followed successfully" });
     }
   } catch (error) {
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -8,7 +8,7 @@ import {
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-//router.get("/suggested", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getUserProfile);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 //router.post("/update", protectRoute, updateUserProfile);
 

f7d258827e5e04f9f48ad33e23f4ba25170e4f1b
imoncrack27 <mingodrexell321@gmail.com>
2025-04-21 15:18:04 +0800

    post text initiated but image is not working hopefully it will work on teh frontend when uploading images

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -30,6 +31,7 @@ app.use(cookieParser()); // to parse cookies
 //Route middleware
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

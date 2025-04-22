3dda79e6228d09986319877a1462e649e24c7080
imoncrack27 <mingodrexell321@gmail.com>
2025-04-22 13:02:30 +0800

    added likeUnlikePost and getAllPosts, but likeUnlikePost has a bug in the code because postman is reading the first if loop

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js";
+import Notification from "../models/notification.model.js";
+
 import { v2 as cloudinary } from "cloudinary";
 
 export const createPost = async (req, res) => {
@@ -83,6 +85,58 @@ export const commentOnPost = async (req, res) => {
     res.status(200).json(post); // return the updated post
   } catch (error) {
     console.log("Error in commentOnPost controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { Id: postId } = req.params;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+      //unlike post
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+      //like post
+      post.likes.push(userId);
+      await post.save();
+
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+      await notification.save();
+
+      res.status(200).json({ message: "Post liked successfully" });
+    }
+  } catch (error) {
+    console.log("Error in likeUnlikePost controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+
+    if (posts.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error("Error in getAllPosts controller: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,12 +4,15 @@ import {
   commentOnPost,
   createPost,
   deletePost,
+  getAllPosts,
+  likeUnlikePost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts);
 router.post("/create", protectRoute, createPost);
-//router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/:id", protectRoute, deletePost);
 export default router;

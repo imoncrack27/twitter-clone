2389a611baadd689b3f8f9606ec3441530444598
imoncrack27 <mingodrexell321@gmail.com>
2025-04-28 14:30:38 +0800

    added route for sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* Common component,bc it's not wrapped with Routes */}
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />

3d99e2184484df12873588d0f6c9b994696b54e2
imoncrack27 <mingodrexell321@gmail.com>
2025-04-29 22:34:34 +0800

    changed the proxy so that we get easier fetch codes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    //This changes the await fetch for easier use
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
   },
 });

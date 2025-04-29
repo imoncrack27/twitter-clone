1e1858b107bd4db5fe2450236a392e12e6d056e4
imoncrack27 <mingodrexell321@gmail.com>
2025-04-29 23:25:43 +0800

    Provider was not inclided on QueryClient "" ""

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClient client={queryClient}>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </QueryClient>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );

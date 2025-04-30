12ac83eda48f076e43a66ae8e7711ea7feb5e31f
imoncrack27 <mingodrexell321@gmail.com>
2025-04-30 20:49:54 +0800

    added queryClient for cache refresh loads

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import XSvg from "../../../components/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
-import toast from "react-hot-toast";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
@@ -14,6 +13,7 @@ const LoginPage = () => {
     password: "",
   });
 
+  const queryClient = useQueryClient();
   const {
     mutate: loginMutation,
     isError,
@@ -39,7 +39,8 @@ const LoginPage = () => {
       }
     },
     onSuccess: () => {
-      toast.success("Login successfully!");
+      //refetch the authUser
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
     },
   });
 

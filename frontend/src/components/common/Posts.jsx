7880a57410020db24ef1a603ecb7b4c738cad27f
imoncrack27 <mingodrexell321@gmail.com>
2025-05-01 01:11:14 +0800

    refactor: replace switch statement with if-else in getPostEndpoint

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,9 +1,37 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
 
-const Posts = () => {
-  const isLoading = false;
+const Posts = ({ feedType }) => {
+  const getPostEndpoint = () => {
+    if (feedType === "forYou") {
+      return "/api/posts/all";
+    } else if (feedType === "following") {
+      return "/api/posts/following";
+    } else {
+      return "/api/posts/all";
+    }
+  };
+
+  const POST_ENDPOINT = getPostEndpoint();
+
+  const { data: posts, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Could not fetch posts.");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
 
   return (
     <>
@@ -14,12 +42,12 @@ const Posts = () => {
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {!isLoading && posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {!isLoading && posts && (
         <div>
-          {POSTS.map((post) => (
+          {posts.map((post) => (
             <Post key={post._id} post={post} />
           ))}
         </div>

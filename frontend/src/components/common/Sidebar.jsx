8557ca91b562b42d5defed0de2d4771e41742eb9
imoncrack27 <mingodrexell321@gmail.com>
2025-04-30 21:03:03 +0800

    Can be logged out

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,10 +5,11 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const Sidebar = () => {
+  const queryClient = useQueryClient();
   const { mutate: logout } = useMutation({
     mutationFn: async () => {
       try {
@@ -26,8 +27,8 @@ const Sidebar = () => {
       }
     },
     onSuccess: () => {
-      toast.success("Logout successfully!");
       // Handle successful logout
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
     },
     onError: () => {
       toast.error("Failed to logout");

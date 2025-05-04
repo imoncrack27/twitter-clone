4eb89310b09139268a1c3f3026dac8aac61a2685
imoncrack27 <mingodrexell321@gmail.com>
2025-05-04 14:03:09 +0800

    added limit to the image upload to 5kb

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(express.json()); // to parse req.body\
+app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse urlencoded data
 
 app.use(cookieParser()); // to parse cookies

38298d6c076040a3977e0c9278409f23392a79bc
imoncrack27 <mingodrexell321@gmail.com>
2025-05-06 14:50:40 +0800

    profile page and user info fetched

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -227,7 +227,7 @@ const ProfilePage = () => {
             </>
           )}
 
-          <Posts />
+          <Posts feedType={feedType} username={username} userId={user?._id} />
         </div>
       </div>
     </>

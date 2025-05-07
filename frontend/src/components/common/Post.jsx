52976b17501b90499fe1d270e1d39f4157d56225
imoncrack27 <mingodrexell321@gmail.com>
2025-05-07 18:59:09 +0800

    removed expressed validator

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,8 +9,6 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
 import LoadingSpinner from "./LoadingSpinner";
-import { query } from "express-validator";
-import { set } from "mongoose";
 import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {

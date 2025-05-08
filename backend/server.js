4926064de731c413bec5cbd527b1062703505bb3
imoncrack27 <mingodrexell321@gmail.com>
2025-05-08 13:37:06 +0800

    new branch

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -36,6 +37,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+app.use("/api/search", searchRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
